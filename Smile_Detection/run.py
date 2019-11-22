67ccaf1b9f2d8e076979472275318cf18c62c2b0
gauravlochab <gauravlochab1997@gmail.com>
2019-11-22 17:21:12 +0530

    Update run.py

--- Smile_Detection/run.py
+++ Smile_Detection/run.py
@@ -33,23 +33,41 @@ cv2.namedWindow("test")
 while True:
     ret, frame = vs.read()
     frame = cv2.resize(frame, (frame.shape[0],450))
+
+    imhsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    imhsvCLAHE = imhsv.copy()
+
+    # Perform histogram equalization only on the V channel
+    imhsv[:,:,2] = cv2.equalizeHist(imhsv[:,:,2])
+
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    imhsvCLAHE[:,:,2] = clahe.apply(imhsvCLAHE[:,:,2])
+
+    # Convert back to BGR format
+    imEq = cv2.cvtColor(imhsv, cv2.COLOR_HSV2BGR)
+    imEqCLAHE = cv2.cvtColor(imhsvCLAHE, cv2.COLOR_HSV2BGR)
+    frame = imEqCLAHE
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     rects = detector(gray, 0)
+    print("1")
     for rect in rects:
         shape = predictor(gray, rect)
         coords = np.zeros((shape.num_parts, 2), dtype='int')
+        print("2")
         for i in range(0, shape.num_parts):
             coords[i] = (shape.part(i).x, shape.part(i).y)
 
         shape = coords
         mouth= shape[mStart:mEnd]
         mar= smile(mouth)
+        print("3")
         mouthHull = cv2.convexHull(mouth)
         #print(shape)
         cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
        
 
-        if mar <= .25 or mar > .40 :
+        if mar <= .29 or mar > .40 :
             COUNTER += 1
             cv2.putText(frame, "SMILE", (200,150), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
         else:
